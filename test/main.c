72302a81b22aa1e09baacf9a53b92b31f49b2c8d
James Alhassan <jamesalhassan052@gmail.com>
2022-10-18 17:14:57 +0000

    main.c updated

--- test/main.c
+++ test/main.c
@@ -9,16 +9,10 @@
  */
 int main(void)
 {
-    int len;
-    int len2;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
    _printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
     return (0);
 }
