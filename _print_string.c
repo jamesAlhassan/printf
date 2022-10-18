c09b27288a1ebb8d3f54ba4f79b128145245593e
James Alhassan <jamesalhassan052@gmail.com>
2022-10-18 14:56:14 +0000

    _print_string updated

--- _print_string.c
+++ _print_string.c
@@ -10,10 +10,13 @@ int _print_string(char *str)
 {
 	int i;
 
+	if (str == NULL)
+		return (0);
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		(write(1, &str[i], 1));
 	}
 
-	return (0);
+	return (i);
 }
