99b24e60963232673ea9f908d0496854a5e11e89
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-10-18 20:10:03 +0000

    Change parameter to va_list

--- _print_char.c
+++ _print_char.c
@@ -2,11 +2,12 @@
 
 /**
  * _print_char - prints a single character
- * @c: character to print
+ * @ls: list of arguments
  * Return: number of characters printed
  */
-int _print_char(char c)
+int _print_char(va_list ls)
 {
-	(write(1, &c, 1));
+	char c = va_arg(ls, int);
+	write(1, &c, 1);
 	return (1);
 }
