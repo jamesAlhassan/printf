dee0f89c9f0d4d3ffe5cf5dea53d97d3cee07634
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-10-18 16:03:19 +0000

    Use write for case c of switch

--- _printf.c
+++ _printf.c
@@ -37,8 +37,9 @@ int _printf(const char *format, ...)
 					count++;
 					break;
 				case 'c':
-					_putchar(va_arg(arg, int));
-					count++;
+					count = count + write(1, &va_arg(arg, int), 1);
+					/** _putchar(va_arg(arg, int)); */
+					/** count++; */
 					break;
 				case 's':
 					count = count + _print_string(va_arg(arg, char *));
