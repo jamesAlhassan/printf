c4e1cf266738f0a48eb5d8f3b95ff96be39e931a
James Alhassan <jamesalhassan052@gmail.com>
2022-10-18 20:16:49 +0000

    _print_char prototype updated

--- main.h
+++ main.h
@@ -18,7 +18,7 @@ typedef struct charfunction
 int _printf(const char *format, ...);
 int _putchar(char c);
 int _print_string(char *str);
-int _print_char(char c);
+int _print_char(va_list ls);
 int _print_int(int a);
 
 #endif
