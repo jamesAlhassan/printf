7a535696510ad567059f91c0e28b03ed526f9cce
James Alhassan <jamesalhassan052@gmail.com>
2022-10-18 17:30:48 +0000

    _printf.c updated

--- test/_printf.c
+++ test/_printf.c
@@ -1,20 +1,16 @@
 #include "main.h"
-
 /**
  * _printf - produces output according to a format
  * @format: is a character string.
  * Return: 0 if success
  */
-
 int _printf(const char *format, ...)
 {
 	int i, count;
-
 	va_list arg;
 
 	if (format == NULL)
 		return (-1);
-
 	count = 0;
 	va_start(arg, format);
 	for (i = 0; format[i] != '\0'; i++)
@@ -27,7 +23,6 @@ int _printf(const char *format, ...)
 		else
 		{
 			i++;
-
 			if (format[i] == '\0')
 				return (0);
 			switch (format[i])
