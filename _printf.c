a115b665c76ed7c126763a9d376f697a110b087a
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2022-10-18 00:30:50 +0000

    Add files

---
+++ _printf.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format
+ * @format: is a character string.
+ * Return: 0 if success
+ */
+
+int _printf(const char *format, ...)
+{
+	int i;
+
+	va_list arg;
+
+	va_start(arg, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+			_putchar(format[i]);
+		else
+		{
+			i++;
+
+			if (format[i] == '\0')
+				return (0);
+
+			switch (format[i])
+			{
+				case '%':
+					_putchar('%');
+					break;
+				case 'c':
+					_putchar(va_arg(arg, int));
+					break;
+			}
+		}
+	}
+	va_end(arg);
+
+	return (0);
+}
